21629db60a8e0b155d37026fa2c9515cc63c939e
Khachatur <casilias35@hotmail.com>
2020-12-02 13:42:00 +0100

    edited oef1 + oef2 nog niet af

--- oef2.1/steden.php
+++ oef2.1/steden.php
@@ -1,6 +1,13 @@
-<?php error_reporting( E_ALL );
+<?php
+
+error_reporting( E_ALL );
 ini_set( 'display_errors', 1 );
+
+include_once "connection.php"
+
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +20,7 @@ ini_set( 'display_errors', 1 );
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     <style>
         @media screen and (max-width: 1001px) {
             .img {
@@ -24,6 +32,7 @@ ini_set( 'display_errors', 1 );
 </head>
 <body>
 
+
 <div class="jumbotron text-center">
     <h1>Top 3 best visited City's</h1>
     <p>Resize this responsive page to see the effect!</p>
@@ -34,15 +43,38 @@ ini_set( 'display_errors', 1 );
     <div class='row'>
 
         <?php
-        $steden = array("london" => "London", "berlin" => "Berlin","paris" => "Paris");
 
-        foreach ($steden as $key => $value ) {
+        function getData($statement){
+global $conn;
+            $result = $conn -> query($statement);
 
-            $img = './img/'.$key.'.jpeg';
+            return $row = $result -> fetch_assoc();
+        };
+        //define and execute query
+
+
+        $sql = "select * from images";
+//        $result = $conn -> query($sql);
+
+
+        // show result if there are many
+        if ($result -> num_rows > 0){
+            // output data of each row
+
+
+            getData($sql);
+            while($row = $result -> fetch_assoc()){
+
+         $steden = array("$row[img_filename]" => "$row[img_title]");
+
+           foreach ($steden as $key => $value ) {
+
+            $img = './img/'.$key;
 
 
             echo "<div class='col-sm-4'>";
             echo "<h3>$value </h3>";
+            echo "$row[img_width] x $row[img_height]" ;
             echo "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>";
             echo "<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>";
             echo "<img class='img' src='$img' width='300px' height='250px'> ";
@@ -50,6 +82,9 @@ ini_set( 'display_errors', 1 );
 
             echo "</div>";
 
+            }
+
+          }
         }
         ?>
     </div>
