840e174a0f13691dd0728b0ed30cfd8e59665654
Khachatur <casilias35@hotmail.com>
2020-12-03 03:01:55 +0100

    put all include files to one

---
+++ oef2.2/lib/include_print.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once "getData.php";
+include_once "lib/errorHandling.php";
+include_once "lib/htmlComponents.php";
+printHead("templates/head.html");
+printHead("templates/jumbo.html");
